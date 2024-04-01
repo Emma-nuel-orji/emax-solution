3aa540614be23859f949f95bdef367fb96fbc4bf
Emma-nuel-orji <eorji452@gmail.com>
2024-04-01 01:07:18 +0100

    first commit

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -9,14 +9,21 @@ admin = Blueprint('admin', __name__)
 @admin.route('/admin', methods=['GET', 'POST'])
 # @login_required
 def admin1():
-    users = User.query.all()
+    if current_user.email != 'orjiobiajulum@yahoo.com':
+        flash("Sorry you have to be an admin to access this page", "info")
+        return redirect(url_for('main.index'))
     product = Product.query.all()
+    users = User.query.all()
+    return render_template("Admin/index.html", product=product, users=users)
+
+    # users = User.query.all()
+    # product = Product.query.all()
     # if current_user.email != 'emeraldinteriorservices@gmail.com':
     #     flash('Please you cant access to this page', 'danger')
     #     return redirect(url_for('main.index', users=users, product=product))
     # else:
     #     render_template('admin/home.html', users=users, product=product)
-    return render_template("admin/index.html", users=users, product=product,)
+    # return render_template("admin/index.html", users=users, product=product,)
 
 
 @admin.route('/users', methods=['GET', 'POST'])
